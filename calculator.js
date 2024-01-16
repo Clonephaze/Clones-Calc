2b81ba21da1f0fc98c0756e7fec406427624181f
Clonephaze <jackdsmith2327@gmail.com>
2024-01-15 21:35:35 -0800

    Chore: restructured the whole calc
    Moved all the buttons around, added parenthesis, and added a bar that shows your whole entry

--- calculator.js
+++ calculator.js
@@ -1,40 +1,48 @@
 let display = document.getElementById('screen');
 let buttons = document.querySelectorAll('button');
+let wholeOpperand = document.getElementById('whole-opperand');
 let mathCollection = '';
+let operationMap = {'+': '+', '-': '-', '/': '/', 'x': '*'};
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
         if (display.getAttribute('data-answered') == 'true') {
-            if (e.target.innerText == '=' || e.target.innerText == '+' || e.target.innerText == '-' || e.target.innerText == 'x' || e.target.innerText == '/') {
+            if (e.target.innerText == '=' || e.target.innerText == '+' || e.target.innerText == '-' || e.target.innerText == 'x' || e.target.innerText == '/' || e.target.innerText == ')') {
                 return
-            } else if (e.target.innerText == 'RESET' || e.target.innerText == 'DEL') {
+            } else if (e.target.innerText == 'AC' || e.target.innerText == 'DEL') {
                 display.innerText = '0';
+                wholeOpperand.innerText = '';
                 display.setAttribute('data-answered', 'false');
             } else {
                 display.innerText = e.target.innerText;
+                wholeOpperand.innerText = '';
+                wholeOpperand.innerText += e.target.innerText;
                 display.setAttribute('data-answered', 'false');
             }
         } else {
             switch(e.target.innerText){
-                case 'RESET':
+                case 'AC':
                     display.innerText = '0';
                     mathCollection = '';
+                    wholeOpperand.innerText = '';
                 break;
                 case 'DEL':
                     if(display.innerText.length == 1){
                         display.innerText = '0';
+                        wholeOpperand.innerText = '';
                     } else {
                         display.innerText = display.innerText.slice(0, -1);
+                        wholeOpperand.innerText = wholeOpperand.innerText.slice(0, -1);
                     }
                 break;
                 case '+':
                 case '-':
                 case '/':
                 case 'x':
-                    let operationMap = {'+': '+', '-': '-', '/': '/', 'x': '*'};
                     mathCollection += display.innerText + operationMap[e.target.innerText];
                     console.log(mathCollection);
                     display.innerText = '0';
+                    wholeOpperand.innerText += ' ' + e.target.innerText + ' ';
                 break;
                 case '=':
                     if (mathCollection.length == 0) {
@@ -46,6 +54,7 @@ buttons.forEach(button => {
                         if (!regex.test(mathCollection + display.innerText)) {
                             display.innerText = 'ERROR';
                             mathCollection = '';
+                            wholeOpperand.innerText = '';
                             display.setAttribute('data-answered', 'true');
                             return;
                         }
@@ -56,6 +65,7 @@ buttons.forEach(button => {
                                 answerStr = answerStr.replace(/(\.\d+?)0+$/, '$1').replace(/\.$/, '');
                                 answer = parseFloat(answerStr);
                                 display.innerText = answer;
+                                wholeOpperand.innerText += ' = ';
                                 mathCollection = '';
                                 display.setAttribute('data-answered', 'true');
                             } else {
@@ -70,12 +80,22 @@ buttons.forEach(button => {
                         }
                     }
                 break;
+                case ')':
+                    if (display.innerText === '0') {
+                        return;
+                    } else {
+                        display.innerText += e.target.innerText;
+                        wholeOpperand.innerText += e.target.innerText;
+                    }
+                break;
                 default:
                     if(display.innerText === '0'){
                         display.innerText = '';
                         display.innerText += e.target.innerText;
+                        wholeOpperand.innerText += e.target.innerText;
                     } else {
                         display.innerText += e.target.innerText;
+                        wholeOpperand.innerText += e.target.innerText;
                     }
                 break;
             }
