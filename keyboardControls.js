9c3b48d7e06b9a602b035ebabcf06e01c8d9c7b6
Clonephaze <jackdsmith2327@gmail.com>
2024-01-17 14:03:57 -0800

    chore: added keyboard controls

---
+++ keyboardControls.js
@@ -0,0 +1,68 @@
+window.addEventListener("keydown", function(event) {
+    switch (event.key) {
+        case "1":
+        case "Numpad1":
+            document.getElementById("1").click();
+        break;
+        case "2":
+        case "Numpad2":
+            document.getElementById("2").click();
+        break;
+        case "3":
+        case "Numpad3":
+            document.getElementById("3").click();
+        break;
+        case "4":
+        case "Numpad4":
+            document.getElementById("4").click();
+        break;
+        case "5":
+        case "Numpad5":
+            document.getElementById("5").click();
+        break;
+        case "6":
+        case "Numpad6":
+            document.getElementById("6").click();
+        break;
+        case "7":
+        case "Numpad7":
+            document.getElementById("7").click();
+        break;
+        case "8":
+        case "Numpad8":
+            document.getElementById("8").click();
+        break;
+        case "9":
+        case "Numpad9":
+            document.getElementById("9").click();
+        break;
+        case "0":
+        case "Numpad0":
+            document.getElementById("0").click();
+        break;
+        case "Enter":
+            document.getElementById("equal").click();
+        break;
+        case "Backspace":
+            document.getElementById("delete").click();
+        break;
+        case "Escape":
+            document.getElementById("AC").click();
+        break;
+        case "*":
+            document.getElementById("multiply").click();
+        break;
+        case "/":
+            document.getElementById("/").click();
+        break;
+        case "-":
+            document.getElementById("-").click();
+        break;
+        case "+":
+            document.getElementById("+").click();
+        break;
+        case ".":
+            document.getElementById(".").click();
+        break;
+    }
+})
